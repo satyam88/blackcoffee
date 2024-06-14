e451bde8e0225663a6a38c9694f0ec82e9ea6fab
satyam88 <satyampandey88@gmail.com>
2024-06-14 12:57:48 +0530

    Update coffee.java

--- coffee.java
+++ coffee.java
@@ -1,3 +1,111 @@
-hello
-  jjkkkk
-hhhh
+FROM tomcat:9.0.52-jre11-openjdk-slim
+
+# Install Java 17
+RUN apt-get update && \
+    apt-get install -y openjdk-17-jdk && \
+    apt-get clean;
+
+# Set Java 17 as the default Java version
+RUN update-alternatives --set java /usr/lib/jvm/java-17-openjdk-amd64/bin/java && \
+    update-alternatives --set javac /usr/lib/jvm/java-17-openjdk-amd64/bin/javac
+
+# Copy the JAR file into the Tomcat webapps directory
+COPY ./target/easymytrip*.jar /usr/local/tomcat/webapps
+
+# Expose port 8080
+EXPOSE 8080
+
+# Set the user
+USER easymytrip
+
+# Set the working directory
+WORKDIR /usr/local/tomcat/webapps
+
+# Start Tomcat
+CMD ["catalina.sh", "run"]
+=================================
+
+pipeline {
+    agent any
+
+    environment {
+        IMAGE_NAME = "satyam88/easymytrip:dev-easymytrip-v.1.${env.BUILD_NUMBER}"
+        ECR_IMAGE_NAME = "533267238276.dkr.ecr.ap-south-1.amazonaws.com/easymytrip:dev-easymytrip-v.1.${env.BUILD_NUMBER}"
+    }
+
+    options {
+        buildDiscarder(logRotator(numToKeepStr: '5', artifactNumToKeepStr: '5'))
+    }
+
+    tools {
+        maven 'maven_3.9.4'
+    }
+
+    stages {
+        stage('Code Compilation') {
+            steps {
+                echo 'Code Compilation is In Progress!'
+                sh 'mvn clean compile'
+                echo 'Code Compilation is Completed Successfully!'
+            }
+        }
+        stage('Code QA Execution') {
+            steps {
+                echo 'JUnit Test Case Check in Progress!'
+                sh 'mvn clean test'
+                echo 'JUnit Test Case Check Completed!'
+            }
+        }
+        stage('Code Package') {
+            steps {
+                echo 'Creating WAR Artifact'
+                sh 'mvn clean package'
+                echo 'WAR Artifact Creation Completed'
+            }
+        }
+        stage('Building & Tag Docker Image') {
+            steps {
+                echo "Starting Building Docker Image: ${env.IMAGE_NAME}"
+                sh "docker build -t ${env.IMAGE_NAME} ."
+                echo 'Docker Image Build Completed'
+            }
+        }
+        stage('Docker Image Scanning') {
+            steps {
+                echo 'Docker Image Scanning Started'
+                // Add actual scanning steps here
+                echo 'Docker Image Scanning Completed'
+            }
+        }
+        stage('Docker Push to Docker Hub') {
+            steps {
+                withCredentials([usernamePassword(credentialsId: 'DOCKER_HUB_CRED', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+                    echo "Pushing Docker Image to DockerHub: ${env.IMAGE_NAME}"
+                    sh "docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}"
+                    sh "docker push ${env.IMAGE_NAME}"
+                    echo "Docker Image Push to DockerHub Completed"
+                }
+            }
+        }
+        stage('Docker Image Push to Amazon ECR') {
+            steps {
+                echo "Tagging Docker Image for ECR: ${env.ECR_IMAGE_NAME}"
+                sh "docker tag ${env.IMAGE_NAME} ${env.ECR_IMAGE_NAME}"
+                echo "Docker Image Tagging Completed"
+
+                withDockerRegistry([credentialsId: 'ecr:ap-south-1:ecr-credentials', url: "https://533267238276.dkr.ecr.ap-south-1.amazonaws.com"]) {
+                    echo "Pushing Docker Image to ECR: ${env.ECR_IMAGE_NAME}"
+                    sh "docker push ${env.ECR_IMAGE_NAME}"
+                    echo "Docker Image Push to ECR Completed"
+                }
+            }
+        }
+        stage('Delete Local Docker Images') {
+            steps {
+                echo "Deleting Local Docker Images: ${env.IMAGE_NAME} and ${env.ECR_IMAGE_NAME}"
+                sh "docker rmi ${env.IMAGE_NAME} ${env.ECR_IMAGE_NAME}"
+                echo "Local Docker Images Deletion Completed"
+            }
+        }
+    }
+}
