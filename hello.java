e706f4ee3d95452a0120a3909a001d05908e846f
satyam88 <satyampandey88@gmail.com>
2024-09-12 12:36:00 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,2 +1,19 @@
-hello
-hi
+what i wanted to say here is instead of maintaing nginx-igress, istio-ingress, istio-service and many other post  aks deployment stuff sepratelly for each pod, can't we create a common repo like layer-kube which should have  helm configuration file for nginx-igress, istio-ingress, istio-service and should have gitlab.yaml which sould created a helm-release-v1.0.zip (auto-incremented ) and gitlab.yaml should have another terraform  stage which should deploy these helm release as terraform resource to all the ATcodes
+
+
+ layer-kube/
+├── helm-charts/
+│   ├── nginx-ingress/
+│   │   └── values.yaml
+│   ├── istio-ingress/
+│   │   └── values.yaml
+│   ├── istio-service/
+│   │   └── values.yaml
+│   └── ...
+├── global-values.yaml
+├── gitlab-ci.yaml
+├── terraform/
+│   └── main.tf
+└── README.md
+
+  We have already Service connection 
