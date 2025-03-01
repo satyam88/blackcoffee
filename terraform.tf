96ccc3cbd99d158e3527e89fc0abc02d363c600d
satyam88 <satyampandey88@gmail.com>
2025-03-01 10:06:44 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,1 +1,73 @@
+Lab1
+=========
+provider "aws" {
+  region = "ap-south-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.86.0"
+    }
+  }
+}
+============
+resource "aws_instance" "myinstance" {
+  ami             = "ami-0b99c7725b9484f9e"
+  instance_type   = "t2.micro"
+  count           = 1
+  key_name        = "aws-2025"
+  security_groups = [aws_security_group.allow_tls.id] # Corrected reference
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block] # Fixed list issue
+  }
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block] # Fixed list issue
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name        = "My Network"
+    Environment = "Dev"
+  }
+}
 
